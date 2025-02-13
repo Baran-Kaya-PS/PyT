302e0f813bb598c61d85d9daa239430f9cd88c0d
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-02-13 15:49:51 +0100

    exercice 2 done ; using input(str) to concat str with the user input then print the user_input variable with .lower() instead of upper

---
+++ BuiltInTypes/str/exercices1-10/2.py
@@ -0,0 +1,10 @@
+# Demande une phrase et affiche-la en minuscule.
+#📌 Pré-requis : Variables, affichage (print()),
+# entrée utilisateur (input()), bases des chaînes (.lower(), .upper(), .capitalize(), etc.).
+#Demande une phrase à l'utilisateur et affiche-la en majuscule.
+
+print("Ecrivez une phrase")
+
+user_input = input("--> ")
+
+print(user_input.lower())
