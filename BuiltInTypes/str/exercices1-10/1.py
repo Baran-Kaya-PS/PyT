89ec20aa24bc2f4f2153c171b94cead092f8ef18
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-02-13 15:48:12 +0100

    exercice 1 done ; using input(str) to concat str with the user input then print the user_input variable with .upper()

---
+++ BuiltInTypes/str/exercices1-10/1.py
@@ -0,0 +1,10 @@
+# Pré-requis : Variables, affichage (print()),
+# entrée utilisateur (input()) bases des chaînes (.lower(), .upper(), .capitalize(), etc.).
+
+# Demande une phrase à l'utilisateur et affiche-la en majuscule.
+
+print("Ecrivez une phrase")
+
+user_input = input("--> ") # concatène le string à l'interieur avec l'entrée utilisateur
+
+print(user_input.upper())
