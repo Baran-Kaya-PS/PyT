f2221ec6f3d5f7e7b8c61f7e760f2753985afa81
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-02-16 20:18:13 +0100

    exercice 17;

---
+++ BuiltInTypes/str/exercices11-20/17.py
@@ -0,0 +1,7 @@
+# Demande un prénom et une ville, puis affiche une phrase du type "Paul habite à Paris."
+
+prenom = input("prénom --> ")
+
+ville = input("ville --> ")
+
+print(prenom+" habite à",ville)
