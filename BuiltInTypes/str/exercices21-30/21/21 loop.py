e0c0427246e847e54317e637a5e94faca580c029
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-02-18 15:42:27 +0100

    exercice 21 made a is_digit function then iteratated through user_input and verify for each digit if it is a digit

--- BuiltInTypes/str/exercices21-30/21/21 loop.py
+++ BuiltInTypes/str/exercices21-30/21/21 loop.py
@@ -1,5 +1,17 @@
+def is_digit2(ch):
+    numbers = [0,1,2,3,4,5,6,7,8,9]
+    intValue = ord(ch) - ord('0')
+    if intValue in numbers:
+        return True
+    return False
+
+isDigit = True
+
 user_input = input("-->")
-is_digit = True
 for ch in user_input:
-    if not ch.isdigit(): is_digit = False
-print(is_digit)
+    if not is_digit2(ch):
+        isDigit = False
+print(isDigit)
+
+
+
