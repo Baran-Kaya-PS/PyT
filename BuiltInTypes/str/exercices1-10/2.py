86bfdb62cddc24965c069a5bf1bb23f9b9eeb27d
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-03-04 14:36:33 +0100

    moved content

--- BuiltInTypes/str/exercices1-10/2.py
+++ BuiltInTypes/str/exercices1-10/2.py
@@ -1,7 +1,6 @@
 # Demande une phrase et affiche-la en minuscule.
 #📌 Pré-requis : Variables, affichage (print()),
 # entrée utilisateur (input()), bases des chaînes (.lower(), .upper(), .capitalize(), etc.).
-#Demande une phrase à l'utilisateur et affiche-la en majuscule.
 
 print("Ecrivez une phrase")
 
