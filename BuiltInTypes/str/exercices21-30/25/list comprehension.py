3ddf2e81c0c59af1d70ff5e26eef53c71d846a23
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-03-05 13:36:08 +0100

    25, used max([(object) for item in list]) list comprehension

---
+++ BuiltInTypes/str/exercices21-30/25/list comprehension.py
@@ -0,0 +1,21 @@
+import re
+
+pattern = r"[\w]+"
+
+phrases = [
+    "Le chat dort.",
+    "Python est un langage de programmation puissant.",
+    "Les montagnes enneigées brillent sous le clair de lune.",
+    "L'hippopotomonstrosesquippedaliophobie est la peur des longs mots.",
+    "Un éléphant dans un magasin de porcelaine fait des ravages.",
+    "Le vent souffle fort dans la vallée encaissée.",
+    "Anticonstitutionnellement est l'un des mots les plus longs de la langue française.",
+    "J'adore résoudre des problèmes d'algorithmique et d'optimisation."
+]
+
+def longest_word(sentence):
+    words = re.findall(pattern,sentence)
+    return max([(len(word),word) for word in words])
+
+for sentences in phrases:
+    print(longest_word(sentences))
