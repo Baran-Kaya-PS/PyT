1d74d18c9f8aaacf41efb4ba11c20c1ddfbc2757
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-04-08 12:38:43 +0200

    Creating and Intersecting/union sets comments

--- data structures/List/25.py
+++ data structures/List/25.py
@@ -1,5 +1,5 @@
 s1 = set("a b c d e f g".split())
 s2 = set("a b c d e f g h i j k l m n o p".split())
 
-print(sorted(s1.intersection(s2)))
+print(sorted(s1.intersection(s2))) # ou s1 & s2
 print(sorted(s1.union(s2))) # ou s1 | s2
