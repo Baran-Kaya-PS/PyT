ad346f67a7fe5da1fa6fbef3d11138cdd4941189
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-04-10 12:17:36 +0200

    33 Matrix Transposition using list comprehension & loops

---
+++ data structures/List/33.py
@@ -0,0 +1,23 @@
+# 33. **Matrix Transposition**
+#     Reuse the concept of a matrix (e.g., 3×4).
+#     Transpose it with `[[row[i] for row in matrix] for i in range(...)]`.
+#     Compare that with `zip(*matrix)`.
+
+l = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [10,11,12]
+]
+
+l2 = [[row[i] for row in l] for i in range(len(l[0]))]
+print(l2)
+
+l2 = []
+
+for i in range(len(l[0])):
+    new_row = []
+    for row in l:
+        new_row.append(row[i])
+    l2.append(new_row)
+print(l2)
