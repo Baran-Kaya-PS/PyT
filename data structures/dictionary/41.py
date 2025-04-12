dc464f8f2f4c57a452b03946f9e3fab8d4f729cd
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-04-12 13:50:34 +0200

    Small inventory dictionary

---
+++ data structures/dictionary/41.py
@@ -0,0 +1,21 @@
+# 41. **Create a Small Inventory (Dictionary)**
+#     Request multiple pairs `(item_name, quantity)` from the user.
+#     Store them in a dictionary, adding up the quantities if the item name already exists.
+#     Print the final dictionary.
+from random import randint
+
+item_name = "Eau Lait Beurre Semoule Riz Pattes Poulet".lower().split()
+quantity = [randint(1,9) for x in range(len(item_name))]
+
+dictionary = dict(zip(item_name,quantity))
+
+print(dictionary)
+
+for x in range(5):
+    ui = input("--> ").lower()
+    if ui in dictionary:
+        dictionary[ui] +=1
+        print(dictionary)
+    else:
+        dictionary[ui] = 1
+        print(dictionary)
