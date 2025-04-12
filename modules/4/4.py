e46e698481c9ba731084204972995d4a0ee282ef
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-04-12 15:24:39 +0200

    __name__ == "__main__" condition
    modu.py contain if __name__ == "__main__": *instruction* and theese instruction only works in modu.py file

---
+++ modules/4/4.py
@@ -0,0 +1,9 @@
+# ### 4. `if __name__ == "__main__":` Condition
+# - In the module, place a conditional block that runs some test or prints output only if the file is called directly.
+# - Make sure this test does **not** run when you import the module.
+#
+# **Key Points:** Structuring a file for dual usage (script and module).
+#
+# ---
+
+import modu
