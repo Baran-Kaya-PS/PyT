ace805c4447de79709f62680558e650e18afe785
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-04-18 14:03:15 +0200

    Appending to a log file using datetime, open(...,"a",...)

---
+++ input_output/8/Appending_to_a_Log_File.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+lf = "log.txt"
+
+msg = input()
+log = "{:%y-%m-%d %H:%M:%S} {}".format(datetime.today(),msg)
+
+print("confirm the msg yes/no")
+confirmation = input().strip()
+while confirmation not in ("yes","no"):
+    print("confirm the msg yes/no")
+    confirmation = input().strip()
+
+
+def addlog(log):
+    with open(lf,'a',encoding="utf-8") as tf:
+        tf.write(f"{log}\n")
+
+
+if confirmation == "yes":
+    addlog(log)
+else :
+    exit(0)
