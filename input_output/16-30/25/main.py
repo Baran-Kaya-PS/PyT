9a6043c76acfd0a6c6cda1f3625e04006c48a327
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-05-11 23:53:44 +0200

    comments (for me)

--- input_output/16-30/25/main.py
+++ input_output/16-30/25/main.py
@@ -1,7 +1,7 @@
 with open("lines.txt","r",encoding="utf-8") as sf:
-    lines = [line.strip().lower() for line in sf]
+    lines = [line.strip().lower() for line in sf] # tableau qui récupère chaque ligne
 
-sorted_lines = sorted(lines)
+sorted_lines = sorted(lines) # tri des lignes
 
 with open("sorted_lines.txt","w",encoding="utf-8") as of:
     for line in sorted_lines:
