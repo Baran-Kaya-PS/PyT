b0b1ae5db722c6f6438b6363f2fe2498994ae092
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-05-12 16:51:49 +0200

    isVariable function

--- input_output/16-30/26/main.py
+++ input_output/16-30/26/main.py
@@ -2,12 +2,20 @@ def isComment(line):
     if '#' in line:
         return True
     return False
+
 def extractComment(line):
     if isComment(line):
         if line.startswith("#"):
             return line
         else:
-            return line[line.index('#')+1::]
+            idx = line.index('#') + 1
+            return line[idx::]
     else:
         return None
+
+def isVariable(line):
+    if '=' in line :
+        return True
+    return False
+
 # with open("data.txt","r",encoding="utf-8") as f:
