ae27cd1c6893d09ac1cb9a18c7526e52d7b5c12b
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2025-05-23 15:49:16 +0200

    moving files

--- input_output/16-35/30/main.py
+++ input_output/16-35/30/main.py
@@ -8,7 +8,7 @@ width = 40
 total = 10000
 for x in range(1,total):
     percentage = (x/total)*100
-    filled = max(1,math.floor(x*width/total))
-    bar = "#" * filled + " "*(width-filled)
-    print(f"\r[{bar}] {percentage:3f}%",end="",flush=True)
+    filled = max(1,math.floor(x*width/total)) # pour avoir un #
+    bar = "#" * filled + " "*(width-filled) # total = filled + (width - filled)
+    print(f"\r[{bar}] {percentage:3f}%",end="",flush=True) # using flush to update the data
     sleep(delay)
